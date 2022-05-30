cbcdd606edd05d5f7c066231db4e5b48e93acc06
wangui gichaga <gichagawangui@gmail.com>
2022-05-30 09:52:14 +0300

    lesson

---
+++ Challenges.py/divisibility.py
@@ -0,0 +1,10 @@
+#Write a program to check if a number is divisible by 5 or 7
+#and checks if both condtions if true then displays true, or checks if one of the conditions is true then displays true
+from unicodedata import name
+
+
+num=int(input("Enter the number: "))
+if (num%5==0) and (num%7==0):
+   print(num,"is divisible by 5 and 7")
+else:
+    print(num,"isn't divisible by 5 and 7")
--- lesson10.py
+++ lesson10.py
@@ -1,7 +1,5 @@
-#Write a program to check if a number is divisible by 5 or 7
-#and checks if both condtions if true then displays true, or checks if one of the conditions is true then displays true
-num=int(input("Enter the number: "))
-if (num%5==0) and (num%7==0):
-   print(num,"is divisible by 5 and 7")
-else:
-    print(num,"isn't divisible by 5 and 7")
+
+#Program that customize user input
+print("Enter your name so that we can customize greetings.")
+name=input("Enter your name: ")
+print("Hello,",name)
